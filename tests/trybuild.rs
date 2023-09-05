f962aadf5d1c632053c9dcb2df9c74fd6434fd18
Sean Lynch <phantom@lynches.ca>
2023-09-05 14:33:27 -0400

    Only run wrong-yield-type test on stable rust
    The emitted error message changes slightly between nightly and stable
    and it is not worth always remembering to ignore the test on one or the
    other.

--- tests/trybuild.rs
+++ tests/trybuild.rs
@@ -17,3 +17,15 @@ fn ui_nightly() {
     t.compile_fail("tests/ui/nightly/fail-*.rs");
     t.pass("tests/ui/nightly/pass-*.rs");
 }
+
+#[test]
+#[cfg_attr(
+    nightly,
+    ignore = "these tests are only supported on rust stable"
+)]
+#[cfg_attr(miri, ignore = "ui tests don't run under miri")]
+fn ui_stable() {
+    let t = trybuild::TestCases::new();
+    t.pass("tests/ui/stable/pass-*.rs");
+    t.compile_fail("tests/ui/stable/fail-*.rs");
+}
