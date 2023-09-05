fca9e6ed41865c2137974ad411e273fe9e368c8f
Sean Lynch <phantom@lynches.ca>
2023-09-05 14:33:27 -0400

    Add test verifying all macro parameters

---
+++ tests/ui/pass/all-params.rs
@@ -0,0 +1,18 @@
+mod dummy {
+    pub use fauxgen::*;
+
+    pub type Yield = u32;
+    pub type Arg = u64;
+    pub type Return = &'static str;
+}
+
+#[fauxgen::generator(
+    crate = crate::dummy,
+    yield = crate::dummy::Yield,
+    arg = crate::dummy::Arg
+)]
+fn gen() -> crate::dummy::Return {
+    "test"
+}
+
+fn main() {}
