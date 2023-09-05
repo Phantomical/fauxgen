2214145e2a6028772f13413049921c862f2f0b13
Sean Lynch <phantom@lynches.ca>
2023-09-05 14:33:27 -0400

    Remove dead code

--- examples/inline.rs
+++ examples/inline.rs
@@ -10,16 +10,3 @@ fn main() {
     let vals: Vec<_> = gen.collect();
     assert_eq!(vals, [32, 5]);
 }
-
-fn test2() {
-    use fauxgen::{gen, GeneratorToken};
-
-    let gen = std::pin::pin!(gen!(|token: GeneratorToken<_>| {
-        token.yield_(5i32).await;
-        token.yield_(6).await;
-        token.yield_(77).await;
-    }));
-
-    let vals: Vec<i32> = gen.collect();
-    assert_eq!(vals, [5, 6, 77]);
-}
